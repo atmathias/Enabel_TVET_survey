7b3c4a6de96c71f05650a5781905b5a07abec372
atmathias <atmathias@gmail.com>
2023-05-30 06:12:09 +0300

    check: df_host_graduate_learning_refugee_language_6

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -259,7 +259,7 @@ df_refugee_graduate_learning_local_language_5 <- df_tool_data %>%
          i.check.current_value = idi_reporting_people_get_on_well,
          i.check.value = "", 
          i.check.issue_id = "logic_c_refugee_graduate_learning_local_language_5",
-         i.check.issue = glue("host_graduate_learning_refugee_language: {host_graduate_learning_refugee_language}, 
+         i.check.issue = glue("refugee_graduate_learning_local_language: {refugee_graduate_learning_local_language}, 
                               idi_reporting_people_get_on_well: {idi_reporting_people_get_on_well}"),
          i.check.other_text = "",
          i.check.checked_by = "MT",
@@ -274,6 +274,32 @@ df_refugee_graduate_learning_local_language_5 <- df_tool_data %>%
 
 add_checks_data_to_list(input_list_name = "logic_output", input_df_name = "df_refugee_graduate_learning_local_language_5")
 
+
+# Host graduate reports having been trying to learn a refugee language, but the majority of the two communities do not get along. i.e.
+# host_graduate_learning_refugee_language = "yes" but idi_reporting_people_get_on_well = "no"
+df_host_graduate_learning_refugee_language_6 <- df_tool_data %>% 
+  filter(host_graduate_learning_refugee_language %in% c("yes"), 
+         idi_reporting_people_get_on_well %in% c("no")) %>% 
+  mutate(i.check.deviceid = deviceid,
+         i.check.type = "change_response",
+         i.check.name = "idi_reporting_people_get_on_well", 
+         i.check.current_value = idi_reporting_people_get_on_well,
+         i.check.value = "", 
+         i.check.issue_id = "logic_c_refugee_graduate_learning_local_language_5",
+         i.check.issue = glue("host_graduate_learning_refugee_language: {host_graduate_learning_refugee_language}, 
+                              idi_reporting_people_get_on_well: {idi_reporting_people_get_on_well}"),
+         i.check.other_text = "",
+         i.check.checked_by = "MT",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "",
+         i.check.reviewed = "",
+         i.check.adjust_log = "",
+         i.check.uuid_cl = "",
+         i.check.so_sm_choices = "") %>% 
+  dplyr::select(starts_with("i.check")) %>%
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = ""))
+
+add_checks_data_to_list(input_list_name = "logic_output", input_df_name = "df_host_graduate_learning_refugee_language_6")
 
 
 
